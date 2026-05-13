76c1090999baa2ff8f1e9ab3376fdf5aff0f2583
pnaprasanna <pnaprasanna@gmail.com>
2026-05-13 20:25:25 +0530

    done

--- bm.py
+++ bm.py
@@ -61,6 +61,7 @@ def md_to_cards(md_file, html_file):
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<link rel="icon" type="image/png" href="fav.svg">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Service Dashboard</title>
 
