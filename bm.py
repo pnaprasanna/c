917a0f2baeec9a18af9ccc38fcaf175bd17545cb
pnaprasanna <pnaprasanna@gmail.com>
2026-05-21 18:49:44 +0530

    Update bm.py

--- bm.py
+++ bm.py
@@ -61,7 +61,7 @@ def md_to_cards(md_file, html_file):
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<link rel="icon" type="image/png" href="fav.svg">
 <script src="https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js"></script>
 
 <title>Service Dashboard</title>
